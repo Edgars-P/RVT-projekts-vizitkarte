57353474a4f117c1d3d72fab2df2e699e6cc8541
Edgars Polis <93377422+Edgars-P@users.noreply.github.com>
2023-03-07 12:29:44 +0000

    Lietas

--- src/routes/auth/admin.tsx
+++ src/routes/auth/admin.tsx
@@ -1,5 +1,6 @@
+import { A } from "@solidjs/router";
 import { Suspense } from "solid-js";
-import { createRouteData, Outlet, useNavigate, useRouteData } from "solid-start";
+import { createRouteData, Outlet, useLocation, useNavigate, useRouteData } from "solid-start";
 import { createServerData$ } from "solid-start/server";
 import { getLogin } from "~/scripts/login";
 
@@ -10,11 +11,18 @@ export function routeData() {
     })
 }
 
+const adminTabs = [
+    { name: "Iesniegtie kontakti", url: "/auth/admin/contacts" },
+    { name: "Bloga ieraksti", url: "/auth/admin/blogs" }
+]
+
 export default function UsersLayout() {
 
     const getLoginResource = useRouteData<typeof routeData>()
 
     const navigate = useNavigate()
+
+    const url = useLocation()
 
     // TODO Login and validate
 
@@ -22,11 +30,25 @@ export default function UsersLayout() {
         <div class="is-max-desktop container">
             <Suspense>
                 {(getLoginResource()?.isAdmin) ? (
-                    <Outlet />
+                    <>
+                        <div class="card-header">
+                            <div class="card-header-title has-background-primary-light">
+                                Administratora panelis
+                            </div>
+                        </div>
+                        <div class="tabs">
+                            <ul>
+                                {adminTabs.map(tab => (
+                                    <li classList={{ "is-active": url.pathname.includes(tab.url) }}><A href={tab.url}>{tab.name}</A></li>
+                                ))}
+                            </ul>
+                        </div>
+                        <Outlet />
+                    </>
                 ) : (
                     <div class="card box">
                         <h1>Pieeja nav atļauta!</h1>
-                        <button class="button" onClick={()=>navigate("/auth/login", {replace: true})}>Uz pierakstīšanās lapu</button>
+                        <button class="button" onClick={() => navigate("/auth/login", { replace: true })}>Uz pierakstīšanās lapu</button>
                     </div>
                 )}
             </Suspense>
--- src/routes/auth/admin/index.tsx
+++ src/routes/auth/admin/index.tsx
@@ -2,17 +2,8 @@ import { A } from "solid-start";
 
 export default function AdminView() {
     return (
-        <div class="card">
-            <div class="card-header">
-                <div class="card-header-title has-background-primary-light">
-                    Administratora panelis
-                </div>
-            </div>
             <div class="card-content">
-                <ul>
-                    <li><A href="./contacts">Iesniegtie kontakti</A></li>
-                </ul>
+                Administratora panelis
             </div>
-        </div>
     )
 }
