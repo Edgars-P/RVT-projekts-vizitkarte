1e8f6dc22628f01b88cead61e11b8775f1e08de5
Edgars Polis <edgars@edgara.id.lv>
2023-03-27 09:27:35 +0300

    Blog skats adminam

--- src/routes/auth/admin/blogs.tsx
+++ src/routes/auth/admin/blogs.tsx
@@ -1,4 +1,4 @@
-import {For, Suspense} from "solid-js"
+import {createSignal, For, Show, Suspense} from "solid-js"
 import {useRouteData} from "solid-start"
 import {createServerData$} from "solid-start/server"
 import {Blogs, knexInstance} from "~/scripts/database"
@@ -14,7 +14,52 @@ export default function BlogView() {
 
 	return (
 		<Suspense fallback={<p>Ielādē...</p>}>
-			<For each={blogs()}>{blog => <p>{JSON.stringify(blog)}</p>}</For>
+			<div class="card-content">
+				<For each={blogs()}>
+					{blog => {
+						const [isEdit, setIsEdit] = createSignal(false)
+						return (
+							<>
+								<div class="card">
+									<div class="card-header">
+										<p class="card-header-title">{blog.name}</p>
+										<button
+											class="card-header-icon"
+											onClick={() => setIsEdit(true)}
+										>
+											<i class="bi bi-pencil-fill"></i>
+										</button>
+										<button
+											class="card-header-icon"
+											onClick={() => alert("TODO")}
+										>
+											<i class="bi bi-trash3-fill"></i>
+										</button>
+									</div>
+									<div class="card-content">
+										<Show
+											when={isEdit()}
+											fallback={<div innerHTML={blog.content} />}
+										>
+											<textarea
+												value={blog.content}
+												style={{width: "100%", height: "30rem"}}
+											></textarea>
+											<button class="button" onClick={() => alert("TODO")}>
+												Saglabāt
+											</button>
+											<button class="button" onClick={() => setIsEdit(false)}>
+												Atcelt
+											</button>
+										</Show>
+									</div>
+								</div>
+								<br />
+							</>
+						)
+					}}
+				</For>
+			</div>
 		</Suspense>
 	)
 }
