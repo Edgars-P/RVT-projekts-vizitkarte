241c2755d57f77e926a75e5b69501afb5d2eb3e7
Edgars Polis <edgars@edgara.id.lv>
2023-04-03 10:01:05 +0300

    Labāka sākumlapa

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,26 @@
-export default function Home() {
+import {A} from "@solidjs/router"
+
+export default function HomePage() {
 	return (
 		<div class="is-max-desktop container">
-			<br />
-			<br />
-			<br />
-			<h1 class="is-size-1 has-text-centered">Laipni lūgti manā mājaslapā!</h1>
+			<div class="card-header has-text-centered is-size-1">
+				Edgara Poļa blogs
+			</div>
+			<div class="card-content">
+				<p>
+					Šis ir mājaslapa, kurā es rakstu par programmēšanu un citiem
+					interesantiem tematiem.
+				</p>
+				<br />
+				<p>
+					Šī mājaslapa ir izveidota izmantojot{" "}
+					<A href="https://solidjs.com/">Solid</A>-bāzes {" "}
+					<A href="https://start.solidjs.com/">Solid Start</A> frameworku, ar{" "}
+					<A href="https://www.typescriptlang.org/">TypeScript</A> valodu,{" "}
+					<A href="https://knexjs.org/">Knex</A> datubāzes bibliotēku un{" "}
+					<A href="https://www.sqlite.org/">SQLite</A> datubāzi.
+				</p>
+			</div>
 		</div>
 	)
 }
--- src/routes/reviews.tsx
+++ src/routes/reviews.tsx
@@ -1,4 +1,4 @@
-import {For, Suspense} from "solid-js"
+import {For, Show, Suspense} from "solid-js"
 import {useRouteData} from "solid-start/data"
 import {
 	createServerAction$,
@@ -7,25 +7,38 @@ import {
 } from "solid-start/server"
 import {knexInstance, Reviews} from "~/scripts/database"
 import "../styles/Reviews.css"
+import {getLogin} from "~/scripts/login"
 
 export function routeData() {
-	return createServerData$(async () =>
+	const submittedReviews = createServerData$(async () =>
 		knexInstance<Reviews>("reviews").orderBy("created_at", "desc").select("*")
 	)
+
+	const loginDetails = createServerData$(async (_, f) => {
+		return await getLogin(f.request)
+	})
+
+	return {submittedReviews, loginDetails}
 }
 
 export default function Feedback() {
-	const [_, {Form}] = createServerAction$(async (formData: FormData) => {
-		await knexInstance<Reviews>("reviews").insert({
-			name: formData.get("name")?.toString() ?? "???",
-			stars: parseInt(formData.get("stars")?.toString() ?? "5"),
-			review: formData.get("review")?.toString() ?? "???",
-		})
+	const [reviewForm, {Form}] = createServerAction$(
+		async (formData: FormData, {request}) => {
+			const user = await getLogin(request)
 
-		return redirect("/reviews")
-	})
+			if (!user) throw new Error("Not logged in")
 
-	const submittedReviews = useRouteData<typeof routeData>()
+			await knexInstance<Reviews>("reviews").insert({
+				name: user.name,
+				stars: parseInt(formData.get("stars")?.toString() ?? "5"),
+				review: formData.get("review") as string,
+			})
+
+			return redirect("/reviews")
+		}
+	)
+
+	const {submittedReviews, loginDetails} = useRouteData<typeof routeData>()
 
 	return (
 		<div class="is-max-desktop container">
@@ -35,35 +48,52 @@ export default function Feedback() {
 						Rakstīt atsauksmi
 					</div>
 				</div>
-				<div class="card-content">
-					<Form>
-						<div class="field">
-							<label class="label">Vārds</label>
-							<input class="input" type="text" name="name" id="name" />
-						</div>
-						<div class="field">
-							<label class="label">Zvaigznes</label>
-							<div class="select">
-								<select name="stars" id="stars">
-									{Array(5)
-										.fill("")
-										.map((_, i) => (
-											<option value={5 - i}>{5 - i}</option>
-										))}
-								</select>
+				<Show
+					when={loginDetails()?.username}
+					fallback={
+						<div class="card-content">
+							<div class="notification">
+								<p>Lūdzu ienākt vai reģistrēties lai rakstītu atsauksmi!</p>
 							</div>
 						</div>
-						<div class="field">
-							<label class="label">Saturs</label>
-							<textarea class="textarea" name="review" id="review"></textarea>
-						</div>
-						<div class="field is-grouped is-grouped-centered">
-							<p class="control">
-								<button class="button is-primary">Iesniegt</button>
-							</p>
-						</div>
-					</Form>
-				</div>
+					}
+				>
+					<div class="card-content">
+						<Show when={reviewForm.error}>
+							<div class="notification is-danger">
+								{reviewForm.error?.message}
+							</div>
+						</Show>
+						<Form>
+							<div class="field">
+								<label class="label">Zvaigznes</label>
+								<div class="select">
+									<select name="stars" id="stars">
+										{Array(5)
+											.fill("")
+											.map((_, i) => (
+												<option value={5 - i}>{5 - i}</option>
+											))}
+									</select>
+								</div>
+							</div>
+							<div class="field">
+								<label class="label">Saturs</label>
+								<textarea class="textarea" name="review" id="review"></textarea>
+							</div>
+							<div class="field is-grouped is-grouped-centered">
+								<p class="control">
+									<button
+										class="button is-primary"
+										disabled={reviewForm.pending}
+									>
+										Iesniegt
+									</button>
+								</p>
+							</div>
+						</Form>
+					</div>
+				</Show>
 				<div class="card-header">
 					<div class="card-header-title has-background-primary-light">
 						Citu atsakuksmes
