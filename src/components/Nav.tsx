9236b9c64952bc9a1b4a84d7e7da356bbdcf1396
Edgars Polis <edgars@edgara.id.lv>
2023-04-03 10:33:20 +0300

    Mobīlais nav strādā

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -45,14 +45,14 @@ export default function Navigation() {
 						class="navbar-burger"
 						aria-label="menu"
 						aria-expanded="false"
-						data-target="navbarBasicExample"
+						data-target="navMenu"
 					>
 						<span aria-hidden="true"></span>
 						<span aria-hidden="true"></span>
 						<span aria-hidden="true"></span>
 					</a>
 				</div>
-				<div class="navbar-menu">
+				<div class="navbar-menu" id="navMenu">
 					<div class="navbar-start">
 						{navEntries.map(e => (
 							<A
@@ -94,6 +94,30 @@ export default function Navigation() {
 					</div>
 				</div>
 			</nav>
+			<script type="text/javascript">
+				{`
+					document.addEventListener('DOMContentLoaded', () => {
+
+						// Get all "navbar-burger" elements
+						const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+					
+						// Add a click event on each of them
+						$navbarBurgers.forEach( el => {
+							el.addEventListener('click', () => {
+					
+								// Get the target from the "data-target" attribute
+								const target = el.dataset.target;
+								const $target = document.getElementById(target);
+					
+								// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+								el.classList.toggle('is-active');
+								$target.classList.toggle('is-active');
+					
+							});
+						});
+					});
+			`}
+			</script>
 		</div>
 	)
 }
