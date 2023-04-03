9b7c55b6b45885e4ce59179d2ef2a7eda531c698
Edgars Polis <edgars@edgara.id.lv>
2023-04-03 10:33:48 +0300

    Noņem paroles maiņu

--- src/routes/auth/user/index.tsx
+++ src/routes/auth/user/index.tsx
@@ -14,24 +14,6 @@ export default function currentUserDetails() {
 		}
 	}, {key: "currentUser"})
 
-	const [isChangingPassword, {Form: ChangePasswordForm}] = createServerAction$(
-		async (form: FormData, {request}) => {
-			const user = await getLogin(request)
-			if (user) {
-				const password = form.get("password") as string
-				const password2 = form.get("password2") as string
-				if (password === password2) {
-					await knexInstance<Users>("users")
-						.where({username: user.username})
-						.update({password: await hashPassword(password)})
-				}
-			} else {
-				throw new Error("Not logged in")
-			}
-		},
-		{invalidate: "currentUser"}
-	)
-
 	return (
 		<div class="card-content">
 			<Suspense fallback={<p>Ielādē...</p>}> 
@@ -40,26 +22,6 @@ export default function currentUserDetails() {
 					<li><b>Lietotājvārds: </b> {currentUser()?.username}</li>
 				</ul>
 			</Suspense>
-			<hr />
-			<ChangePasswordForm>
-				<div class="field">
-					<label class="label">Jaunā parole</label>
-					<div class="control">
-						<input class="input" type="password" name="password" />
-					</div>
-				</div>
-				<div class="field">
-					<label class="label">Atkārtot jauno paroli</label>
-					<div class="control">
-						<input class="input" type="password" name="password2" />
-					</div>
-				</div>
-				<div class="field">
-					<div class="control">
-						<button class="button is-link" disabled={isChangingPassword.pending}>Mainīt paroli</button>
-					</div>
-				</div>
-			</ChangePasswordForm>
 		</div>
 	)
 
